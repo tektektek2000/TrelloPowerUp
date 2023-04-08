4e8da9c85753335ca7b515f2ebaac0b338dd9fff
Norbert László <laszlo.norbi2000@hotmail.com>
2023-04-09 01:43:03 +0200

    Changing logging to all.

--- js/client.js
+++ js/client.js
@@ -9,10 +9,10 @@ TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
     return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
-        console.log("We just loaded the card name for fun: " + cardName);
+      .card("all")
+      .get("all")
+      .then(function (card) {
+        console.log("We just loaded the card name for fun: " + card);
         return [
           {
             // It's best to use static badges unless you need your
