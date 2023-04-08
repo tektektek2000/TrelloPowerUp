7dd2f9fa2f42f16a48e03be812cd25d28bef61d5
Norbert László <laszlo.norbi2000@hotmail.com>
2023-04-09 01:38:50 +0200

    Changing a few things to see it working.

--- js/client.js
+++ js/client.js
@@ -15,27 +15,11 @@ TrelloPowerUp.initialize({
         console.log("We just loaded the card name for fun: " + cardName);
         return [
           {
-            // Dynamic badges can have their function rerun
-            // after a set number of seconds defined by refresh.
-            // Minimum of 10 seconds.
-            dynamic: function () {
-              // we could also return a Promise that resolves to
-              // this as well if we needed to do something async first
-              return {
-                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
-                icon: "./images/icon.svg",
-                color: "green",
-                refresh: 10, // in seconds
-              };
-            },
-          },
-          {
             // It's best to use static badges unless you need your
             // badges to refresh.
             // You can mix and match between static and dynamic
             text: "Static",
-            icon: HYPERDEV_ICON, // for card front badges only
-            color: null,
+            color: "green"
           },
         ];
       });
