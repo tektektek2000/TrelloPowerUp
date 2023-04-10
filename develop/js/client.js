7f4d67373d6a2e8b722b8c5559c698da777b8623
Norbert László <laszlo.norbi2000@gmail.com>
2023-04-10 20:38:17 +0000

    deploy

--- develop/js/client.js
+++ develop/js/client.js
@@ -3,24 +3,24 @@ import * as api from "./api.js"
 
 var Promise = TrelloPowerUp.Promise;
 
-function ToTwoDigit(num){
+function ToTwoDigit(num) {
     return num < 10 ? `0${num}` : num;
 }
 
 function showIframe(t) {
     return t.popup({
-      title: 'Authorize to continue',
-      url: 'views/authorize.html'
+        title: 'Authorize to continue',
+        url: 'views/authorize.html'
     });
-  }
-  
-  function showNewMenu(t) {
+}
+
+function showNewMenu(t) {
     return t.modal({
         title: 'New Meeting',
         url: t.signUrl('views/new-meeting.html'),
         fullscreen: false
     });
-  }
+}
 
 TrelloPowerUp.initialize({
     // Start adding handlers for your capabilities here!
@@ -29,52 +29,83 @@ TrelloPowerUp.initialize({
             .card("all")
             .then(function (card) {
                 return t.get(card.id, 'shared', 'meetingCard')
-                .then( cardRole => {
-                    if(cardRole && cardRole.role === "Summary"){
-                        var durationHours = cardRole.startHour < cardRole.endHour ? cardRole.endHour - cardRole.startHour : 24 - cardRole.startHour + cardRole.endHour;
-                        var durationMinutes = cardRole.endMinutes - cardRole.startMinutes;
-                        if(durationMinutes < 0){
-                            durationHours--;
-                            durationMinutes += 60;
-                        }
-                        console.log(cardRole);
-                        return [
-                            {
-                                text: `🕒 ${ToTwoDigit(cardRole.startHour)}:${ToTwoDigit(cardRole.startMinutes)}-${ToTwoDigit(cardRole.endHour)}:${ToTwoDigit(cardRole.endMinutes)}`,
-                                color: "blue"
-                            },
-                            {
-                                text: `Length: ${durationHours} h ${durationMinutes} m`,
-                                color: "light-gray"
+                    .then(cardRole => {
+                        if (cardRole && cardRole.role === "Summary") {
+                            var durationHours = cardRole.startHour < cardRole.endHour ? cardRole.endHour - cardRole.startHour : 24 - cardRole.startHour + cardRole.endHour;
+                            var durationMinutes = cardRole.endMinutes - cardRole.startMinutes;
+                            if (durationMinutes < 0) {
+                                durationHours--;
+                                durationMinutes += 60;
                             }
-                        ];
-                    }
-                    return [];
-                });
+                            console.log(cardRole);
+                            return [
+                                {
+                                    text: `🕒 ${ToTwoDigit(cardRole.startHour)}:${ToTwoDigit(cardRole.startMinutes)}-${ToTwoDigit(cardRole.endHour)}:${ToTwoDigit(cardRole.endMinutes)}`,
+                                    color: "blue"
+                                },
+                                {
+                                    text: `Length: ${durationHours} h ${durationMinutes} m`,
+                                    color: "light-gray"
+                                }
+                            ];
+                        }
+                        return [];
+                    });
             });
     },
     'board-buttons': function (t, opts) {
         return t.getRestApi()
-    	// We now have an instance of the API client.
-        .isAuthorized()
-        .then(function(isAuthorized) {
-            if (isAuthorized) {
-                return [{
-                // we can either provide a button that has a callback function
-                text: 'New Meeting',
-                condition: "edit",
-                callback: showNewMenu
-            }];
-        } else {
-            return [{
-                text: 'New Meeting',
-                condition: "edit",
-                callback: showIframe
-            }];
-        }
-        })
+            // We now have an instance of the API client.
+            .isAuthorized()
+            .then(function (isAuthorized) {
+                if (isAuthorized) {
+                    return [{
+                        // we can either provide a button that has a callback function
+                        text: 'New Meeting',
+                        condition: "edit",
+                        callback: showNewMenu
+                    }];
+                } else {
+                    return [{
+                        text: 'New Meeting',
+                        condition: "edit",
+                        callback: showIframe
+                    }];
+                }
+            })
+    },
+    'card-back-section': function (t, options) {
+        return t
+            .card("all")
+            .then(function (card) {
+                return t.get(card.id, 'shared', 'meetingCard')
+                    .then(cardRole => {
+                        if (cardRole && cardRole.role === "Summary") {
+                            var durationHours = cardRole.startHour < cardRole.endHour ? cardRole.endHour - cardRole.startHour : 24 - cardRole.startHour + cardRole.endHour;
+                            var durationMinutes = cardRole.endMinutes - cardRole.startMinutes;
+                            if (durationMinutes < 0) {
+                                durationHours--;
+                                durationMinutes += 60;
+                            }
+                            console.log(window.location.origin + "/icons/summary.jpg");
+                            return {
+                                title: 'Summary Card',
+                                icon: window.location.origin + "/icons/summary.jpg", // Must be a gray icon, colored icons not allowed.
+                                content: {
+                                    type: 'iframe',
+                                    url: t.signUrl('../views/summary-section.html'),
+                                    height: 500, // Max height is 1500.
+                                },
+                                action: {
+                                    text: 'Remove Summary',
+                                    callback: (t) => {},
+                                }
+                            }
+                        }
+                    })
+            })
     }
-},{
+}, {
     appKey: '2905a45608f989a24bf26e3d92edcf80',
     appName: 'Test'
 });
