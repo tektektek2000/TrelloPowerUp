93f4c465c935fa5d374db0608d49a3707af9f4fb
Norbert László <laszlo.norbi2000@gmail.com>
2023-04-10 23:14:19 +0000

    deploy

--- develop/js/summary-section.js
+++ develop/js/summary-section.js
@@ -13,10 +13,12 @@ $(document).ready(function(){
         appKey: '2905a45608f989a24bf26e3d92edcf80',
         appName: 'Test'
     });
+    $("#meetingstart")[0].value = `23:00`
     console.log(t.getContext());
     t.get(t.getContext().card.id, 'shared', 'meetingCard')
     .then(cardRole => {
         if (cardRole && cardRole.role === "Summary") {
+            console.log(cardRole);
             $("#meetingstart")[0].value = `${ToTwoDigit(cardRole.startHour)}:${ToTwoDigit(cardRole.startMinutes)}`
             $("#meetingend")[0].value = `${ToTwoDigit(cardRole.endHour)}:${ToTwoDigit(cardRole.endMinutes)}`
         }
