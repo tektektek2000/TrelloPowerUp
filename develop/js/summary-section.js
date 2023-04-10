9adfb6009823057897e4765da0da77cbf7775ba6
Norbert László <laszlo.norbi2000@gmail.com>
2023-04-10 23:46:43 +0000

    deploy

--- develop/js/summary-section.js
+++ develop/js/summary-section.js
@@ -2,25 +2,24 @@ function ToTwoDigit(num) {
     return num < 10 ? `0${num}` : num;
 }
 
-$(document).ready(function(){
-    $('#meetingstart').datetimepicker({
-        format: 'hh:mm'
-    });
-    $('#meetingend').datetimepicker({
-        format: 'hh:mm'
-    });
+$(document).ready(function(){  
     var t = window.TrelloPowerUp.iframe({
         appKey: '2905a45608f989a24bf26e3d92edcf80',
         appName: 'Test'
     });
-    $("#meetingstart")[0].value = `23:00`
     console.log(t.getContext());
     t.get('card', 'shared', 'meetingCard')
     .then(cardRole => {
         if (cardRole && cardRole.role === "Summary") {
             console.log(cardRole);
-            $("#meetingstart")[0].value = `${ToTwoDigit(cardRole.startHour)}:${ToTwoDigit(cardRole.startMinutes)}`
-            $("#meetingend")[0].value = `${ToTwoDigit(cardRole.endHour)}:${ToTwoDigit(cardRole.endMinutes)}`
+            $('#meetingstart').datetimepicker({
+                format: 'HH:mm',
+                date: new Date(2016, 9 , 17, cardRole.startHour, cardRole.startMinutes)
+            });
+            $('#meetingend').datetimepicker({
+                format: 'HH:mm',
+                date: new Date(2016, 9 , 17, cardRole.endHour, cardRole.endMinutes)
+            });
         }
     })
 });
