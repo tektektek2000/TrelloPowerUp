15d5603ed7a673e241e1abebf050c4771a72ce11
Norbert László <laszlo.norbi2000@hotmail.com>
2023-04-11 01:09:13 +0200

    Idk

--- js/summary-section.js
+++ js/summary-section.js
@@ -4,17 +4,16 @@ function ToTwoDigit(num) {
 
 $(document).ready(function(){
     $('#meetingstart').datetimepicker({
-        format: 'hh:mm',
-        inline: true
+        format: 'hh:mm'
     });
     $('#meetingend').datetimepicker({
-        format: 'hh:mm',
-        inline: true
+        format: 'hh:mm'
     });
     var t = window.TrelloPowerUp.iframe({
         appKey: '2905a45608f989a24bf26e3d92edcf80',
         appName: 'Test'
     });
+    console.log(t.getContext());
     t.get(t.getContext().card.id, 'shared', 'meetingCard')
     .then(cardRole => {
         if (cardRole && cardRole.role === "Summary") {
