41c08952a49973508367d3ca5e5acc2e155d63e5
Norbert László <laszlo.norbi2000@hotmail.com>
2023-04-10 19:11:33 +0200

    Is this the issue now???????

--- js/client.js
+++ js/client.js
@@ -29,7 +29,7 @@ TrelloPowerUp.initialize({
                         return [
                             {
                                 text: "Summary",
-                                color: "grey"
+                                color: "green"
                             },
                         ];
                     }
