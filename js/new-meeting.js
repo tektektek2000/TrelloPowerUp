c36b049e0884691f45b9fc8abe437a167b90ce0d
Norbert László <laszlo.norbi2000@hotmail.com>
2023-04-10 13:47:25 +0200

    Added appropiate naming for the meeting

--- js/new-meeting.js
+++ js/new-meeting.js
@@ -14,7 +14,12 @@ $(document).ready(function(){
         });
         event.preventDefault();   
         var context = t.getContext();
-        var listName = "test";
+        var listName =  $("#meetingstart")[0].value  + "-" + $("#meetingend")[0].value ;
+        const regex = /(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d) ([ap]m)-(\d\d):(\d\d) ([ap]m)/i;
+        const match = listName.match(regex);
+        var starthour = match[4] === "12" ? (match[6] === "am" ? 0 : 12) : (match[6] === "am" ? parseInt(match[4]) : parseInt(match[4]) + 12);
+        var endhour = match[7] === "12" ? (match[9] === "am" ? 0 : 12) : (match[9] === "am" ? parseInt(match[7]) : parseInt(match[7]) + 12);
+        listName = $("#meetingName")[0].value + " " + `${match[1]}/${match[2]}/${match[3]} ${starthour}:${match[5]}-${endhour}:${match[8]}`;
         t.getRestApi()
         .getToken()
         .then(function(token) {
