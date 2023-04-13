05a7a8b20d4110f25af47493424b3fcb021e8508
Norbert László <laszlo.norbi2000@gmail.com>
2023-04-13 01:30:42 +0000

    deploy

--- develop/js/client.js
+++ develop/js/client.js
@@ -37,8 +37,9 @@ function getListTopicCardsDuration(t, listID){
             return api.getCardsFromList(listID,'2905a45608f989a24bf26e3d92edcf80',token)
             .then(cards => {  
                 var sum = 0;            
+                var promises = []
                 for(const card of cards){
-                    t.get(card.id, 'shared', 'meetingCard')
+                    promises += t.get(card.id, 'shared', 'meetingCard')
                     .then(cardRole => {
                         if (cardRole && cardRole.role === "Topic") {
                             return cardRole.hours * 60 + cardRole.minutes;
@@ -49,7 +50,9 @@ function getListTopicCardsDuration(t, listID){
                         sum += val;
                     })
                 }
-                return sum;
+                return Promise.all(promises).then(() => {
+                    return sum;
+                });
             })
         });
 }
